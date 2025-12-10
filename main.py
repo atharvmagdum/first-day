54e8cf6a7a25443290b74447db79fc71a73951eb
atharvmagdum <atharvmagdum1008@gmail.com>
2025-12-10 23:09:28 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import arithmetic as arith
-print("Hello World !!")
-
-num1 = int(input("Enter first number :"))
-num2 = int(input("Enter Second number :"))
-
-arith.add(num1,num2)
-arith.sub(num1,num2)
