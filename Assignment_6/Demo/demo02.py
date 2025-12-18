dca37f616e93ae796002fc159181e311a405cfa8
atharv magdum <atharvmagdum1008@gmail.com>
2025-12-18 08:54:15 +0530

    Day 5

---
+++ Assignment_6/Demo/demo02.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import json
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = "dummy-key"
+url = "http://127.0.0.1:1234/v1/chat/completions"
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
+}
+
+while True:
+    user_prompt = input("Ask anything: ")
+    if user_prompt == "exit":
+        break
+    # phi 4 mini --> Super Slow
+    # Google Gemma-3 --> Slow
+    # OpenAI's GPT 20B --> Fast
+    req_data = {
+        "model": "google/gemma-3-4b",
+        "messages": [
+            { "role": "user", "content": user_prompt }
+        ],
+    }
+    time1 = time.perf_counter()
+    response = requests.post(url, data=json.dumps(req_data), headers=headers)
+    time2 = time.perf_counter()
+    print("Status:", response.status_code)
+    # print(response.json())
+    resp = response.json()
+    print(resp["choices"][0]["message"]["content"])
+    print(f"Time required: {time2-time1:.2f} sec")
